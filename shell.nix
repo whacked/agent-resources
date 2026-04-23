f20fab3134953c24aa84c16501370c526490031a
whacked <whacked@users.noreply.github.com>
2026-04-23 16:47:27 +0800

    add cpd dep to shell env (wip version)

--- shell.nix
+++ shell.nix
@@ -1,14 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, cpd ? null }:
 let
   # provides "echo-shortcuts"
   nix_shortcuts = import (pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/whacked/setup/ce9fe9be8e42db9ce003772099d08395358efe8c/bash/nix_shortcuts.nix.sh";
     hash = "sha256-uK+Fgwr6iWXbfi/itJGELzkWqGZsQ8HFpfc+ztGSF98=";
   }) { inherit pkgs; };
+
+  cpdPkg = if cpd != null then cpd
+    else (builtins.getFlake "github:whacked/cpd/33997e210f8cb975cf9a356c80ed02399a2922f1").packages.${pkgs.system}.default;
+
 in pkgs.mkShell {
   buildInputs = [
     pkgs.cue
     pkgs.nodejs
+    cpdPkg
   ];  # join lists with ++
 
   nativeBuildInputs = [
