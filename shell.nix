351a191ede1d42b8b23ed6f365800d50435102c0
whacked <whacked@users.noreply.github.com>
2026-05-17 23:36:44 +0800

    add git shortcuts + generate gitignore

--- shell.nix
+++ shell.nix
@@ -5,6 +5,10 @@ let
     url = "https://raw.githubusercontent.com/whacked/setup/ce9fe9be8e42db9ce003772099d08395358efe8c/bash/nix_shortcuts.nix.sh";
     hash = "sha256-uK+Fgwr6iWXbfi/itJGELzkWqGZsQ8HFpfc+ztGSF98=";
   }) { inherit pkgs; };
+  gitShortcuts = (builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/whacked/setup/599e7f6343a80a58ebb1204e305b19e86ce8483e/bash/git_shortcuts.sh";
+    sha256 = "1s3ybj9lwxkm80m8krh0084i5jill7bm37dcl3yirzm6r3alpai4";
+  });
 
   cpdPkg = if cpd != null then cpd
     else (builtins.getFlake "github:whacked/cpd/544740fcaaef8ca474d79f26259b324e1ddabd44").packages.${pkgs.system}.default;
@@ -80,6 +84,7 @@ in pkgs.mkShell {
   ];  # join lists with ++
 
   nativeBuildInputs = [
+    gitShortcuts
   ];
 
   shellHook = nix_shortcuts.shellHook + ''
